0374019a4f62d71b5f204f47d851cbecd6acde84
Viresh Kumar Sharma <vireshsha@gmail.com>
2020-11-10 12:35:35 -0500

    Minor changes into JSON parsing implementation.

--- HomeProject/ViewControllers/SwiftNewsListViewController.swift
+++ HomeProject/ViewControllers/SwiftNewsListViewController.swift
@@ -19,58 +19,58 @@ override func viewDidLoad() {
 }
 
 // MARK: - Download Json Method
-func downloadJson() {
-    
-    if let jsonURL = URL(string: redditURL) {
-      jsonURL.asyncDownload { [self] data, response, error in
-        guard let data = data else {
-           //print("URLSession dataTask error:", error ?? "nil")
-            return
-        }
-        do {
-            let jsonObject = try JSONSerialization.jsonObject(with: data)
-            if let dictionary = jsonObject as? [String: Any],
-               let dataDic = dictionary["data"] as? [String:Any],
-               let results = dataDic["children"] as? [[String: Any]] {
-               parseJson(results: results)
+    // MARK: - Download Json Method
+    func downloadJson() {
+        if let jsonURL = URL(string: redditURL) {
+          jsonURL.asyncDownload { [self] data, response, error in
+            guard let data = data else {
+               //print("URLSession dataTask error:", error ?? "nil")
+                return
             }
-        } catch {
-           // print("JSONSerialization error:", error)
+            do {
+                let jsonObject = try JSONSerialization.jsonObject(with: data)
+                if let dictionary = jsonObject as? [String: Any],
+                   let dataDic = dictionary["data"] as? [String:Any],
+                   let results = dataDic["children"] as? [[String: Any]] {
+                    DispatchQueue.main.async {
+                        parseJson(results: results)
+                    }
+                }
+            } catch {
+               // print("JSONSerialization error:", error)
+            }
         }
+       }
     }
-   }
-}
 
 // MARK: - Json Parsing Method
 func parseJson(results: [[String: Any]])  {
-      results.forEach {child in
-           let dataDic = child["data"] as? [String:Any]
-           do{
-            let jData = try JSONSerialization.data(withJSONObject: dataDic!, options: JSONSerialization.WritingOptions.prettyPrinted)
-            let articleJson = try JSONDecoder().decode(Article.self, from:jData)
-            self.Articles.append(articleJson)
-            
-            // creating a blank image for not received URLS from JSON
-            var image = UIImage(named: "placeholder.jpg")
-            
-            if((dataDic?["thumbnail"] as? String) != nil && dataDic?["thumbnail"] as? String != "self" && dataDic?["thumbnail"] as? String != "default") {
-                //print("imageString= \(String(describing: dataDic?["thumbnail"]))")
-                let imageUrl = URL(string: dataDic!["thumbnail"] as! String)!
-               // print("imageURL= \(imageUrl)")
-                let imageData = try! Data(contentsOf: imageUrl)
-                image = UIImage(data: imageData)
-            }
-            self.imagesArray.append(image)
-           // print("imagesArray = \(self.imagesArray)")
-            }catch let error as NSError  {
-               print("error:", error)
-             }
-           }
-DispatchQueue.main.async {
-       // print("Articles-Array= \(self.Articles)")
-       self.tableView.isHidden = false;
-       self.tableView.reloadData()
-    }
+  results.forEach {child in
+       let dataDic = child["data"] as? [String:Any]
+       do{
+        let jData = try JSONSerialization.data(withJSONObject: dataDic!, options: JSONSerialization.WritingOptions.prettyPrinted)
+        let articleJson = try JSONDecoder().decode(Article.self, from:jData)
+        self.Articles.append(articleJson)
+        
+        // creating a blank image for not received URLS from JSON
+        var image = UIImage(named: "placeholder.jpg")
+           if let url = NSURL(string: dataDic!["thumbnail"] as! String) {
+               if (UIApplication.shared.canOpenURL(url as URL)) {
+                   let imageUrl = URL(string: dataDic!["thumbnail"] as! String)
+                     // print("imageURL= \(String(describing: imageUrl))")
+                    let imageData = try Data(contentsOf: imageUrl!)
+                    image = UIImage(data: imageData)
+                }
+              }
+        self.imagesArray.append(image)
+       // print("imagesArray = \(self.imagesArray)")
+        }catch let error as NSError  {
+             print("error:", error)
+         }
+       }
+   // print("Articles-Array= \(self.Articles)")
+   self.tableView.isHidden = false;
+   self.tableView.reloadData()
 }
 
 // MARK: - Prepare Segue Method
